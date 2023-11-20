cce0ad68bc7f4d620778240d5b4710698dd13d5e
Oh Junseok <y22zzp@naver.com>
2023-11-20 17:03:01 +0900

    JavaScript 입문과 웹 UI개발 for 반복문으로 탭기능 코드 줄여보기

--- JavaScript 입문과 웹 UI개발/tab.js
+++ JavaScript 입문과 웹 UI개발/tab.js
@@ -4,6 +4,34 @@
 // - 모드 div에 붙은 show 클래스명 제거
 // - div0에 show 클래스명 추가 
 
-$('.tab-button').eq(0).on('click', function(){
+// var 버튼 = $('.tab-button');
+//   $('.tab-button').eq(0).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(0).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(0).addClass('show');
+// });
 
-});
+// $('.tab-button').eq(1).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(1).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(1).addClass('show');
+// });
+
+// $('.tab-button').eq(2).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(2).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(2).addClass('show');
+// });
+
+// for 반복문 쓰는 법 
+for (let i = 0; i<3; i++){
+  $('.tab-button').eq(i).on('click', function(){
+    $('.tab-button').removeClass('orange');
+    $('.tab-button').eq(i).addClass('orange');
+    $('.tab-content').removeClass('show');
+    $('.tab-content').eq(i).addClass('show');
+  });
+}
