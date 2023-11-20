4cbf67ba5c16af3f744b61642debbbd2b3c2e0a2
Oh Junseok <y22zzp@naver.com>
2023-11-20 21:14:13 +0900

    JavaScript 입문과 웹 UI개발 데이터바인딩 숙제 & 문자중간에 변수넣는 법

--- JavaScript 입문과 웹 UI개발/tab.js
+++ JavaScript 입문과 웹 UI개발/tab.js
@@ -41,12 +41,3 @@ document.querySelector('.car-title').innerHTML =car2.name
 document.querySelector('.car-price').innerHTML =car2.price[0]
 
 console.log(car2['price'])
-
-let products = [
-  { id : 0, price : 70000, title : 'Blossom Dress' },
-  { id : 1, price : 50000, title : 'Springfield Shirt' },
-  { id : 2, price : 60000, title : 'Black Monastery' }
-];
-
-console.log(products[0].title)
-console.log(products[1].title)
