232f093212ce0fb4ff3d90ced710a1ab64274766
Oh Junseok <y22zzp@naver.com>
2023-11-20 18:17:02 +0900

    JavaScript 입문과 웹 UI개발 이벤트 버블링 응용과 dataset

--- JavaScript 입문과 웹 UI개발/tab.js
+++ JavaScript 입문과 웹 UI개발/tab.js
@@ -1,37 +1,31 @@
-// 버튼 0 누르면 
-// - 모든 버튼에 붙은 orange 클래스명 제거
-// - 버튼 0에 orange 클래스명 추가
-// - 모드 div에 붙은 show 클래스명 제거
-// - div0에 show 클래스명 추가 
-
-// var 버튼 = $('.tab-button');
-//   $('.tab-button').eq(0).on('click', function(){
-//   $('.tab-button').removeClass('orange');
-//   $('.tab-button').eq(0).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(0).addClass('show');
-// });
-
-// $('.tab-button').eq(1).on('click', function(){
-//   $('.tab-button').removeClass('orange');
-//   $('.tab-button').eq(1).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(1).addClass('show');
-// });
-
-// $('.tab-button').eq(2).on('click', function(){
-//   $('.tab-button').removeClass('orange');
-//   $('.tab-button').eq(2).addClass('orange');
-//   $('.tab-content').removeClass('show');
-//   $('.tab-content').eq(2).addClass('show');
-// });
 
 // for 반복문 쓰는 법 
-for (let i = 0; i<3; i++){
-  $('.tab-button').eq(i).on('click', function(){
-    $('.tab-button').removeClass('orange');
-    $('.tab-button').eq(i).addClass('orange');
-    $('.tab-content').removeClass('show');
-    $('.tab-content').eq(i).addClass('show');
-  });
+// for (let i = 0; i<3; i++){
+//   $('.tab-button').eq(i).on('click', function(){
+//    탭열기(i);
+//   });
+// }
+
+// $('.list').click(function(e){
+//   if(e.target == document.querySelectorAll('.tab-button')[0]){
+//     탭열기(0)
+//   }
+//   if(e.target == document.querySelectorAll('.tab-button')[1]){
+//     탭열기(1)
+//   }
+//   if(e.target == document.querySelectorAll('.tab-button')[2]){
+//     탭열기(2)
+//   }
+// })
+
+$('.list').click(function(e){
+  console.log(e.target.dataset.id);
+  탭열기(e.target.dataset.id);
+})
+
+function 탭열기(숫자) {
+  $('.tab-button').removeClass('orange');
+  $('.tab-button').eq(숫자).addClass('orange');
+  $('.tab-content').removeClass('show');
+  $('.tab-content').eq(숫자).addClass('show'); 
 }
