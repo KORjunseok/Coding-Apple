69b4a2f13a98d9ff0644f2b642590dc64b2e4316
Oh Junseok <y22zzp@naver.com>
2023-11-28 18:18:43 +0900

    빠르게 마스터하는 타입스크립트 타입을 미리 정하기 애매할 때 (union type, any, unknown)

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,6 +1,30 @@
-var 이름 = 'kim';
-이름 = "가나다라";
-function 함수(x) {
-    return x * 2;
-}
-함수(1234);
+var 오준석 = {
+    name: "오준석",
+    age: 29,
+    출생지역: "인천"
+};
+var 좋아요 = {
+    제목: "예뻤어",
+    가수: "데이식스"
+};
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
+var 회원들 = [1, "2", 3];
+var 오부젝트 = { a: '123' };
+var 이름;
+// 숙제1 
+// let user:string = 'kim';
+// let age:undefined = undefined;
+// let married:boolean = false; 
+// let 철수:(string|boolean)[] = [user, age, married];
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: 'Phil',
+    friend: 'John'
+};
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher];
+console.log(학교.score);
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -14,3 +14,30 @@ let project:{member:string[],days:number, started:boolean } = {
   days : 30,
   started : true,
 }
+
+let 회원들 :(number|string)[] = [1,"2",3]
+let 오부젝트 :{a : string | number} = {a : '123'}
+
+let 이름:any;
+
+// 숙제1 
+// let user:string = 'kim';
+// let age:undefined = undefined;
+// let married:boolean = false; 
+// let 철수:(string|boolean)[] = [user, age, married];
+
+let 학교 : {
+  score : (number|boolean)[],
+  teacher : string,
+  friend : string | string[]
+}
+
+= {
+  score : [100, 97, 84],
+  teacher : 'Phil',
+  friend : 'John'
+}
+학교.score[4] = false;
+학교.friend = ['Lee', 학교.teacher]
+
+console.log(학교.score)
