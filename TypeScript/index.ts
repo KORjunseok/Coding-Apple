b18e7b2dead75fbf0fc8d8b9147e63f82b0f86f7
Oh Junseok <y22zzp@naver.com>
2023-11-28 19:16:29 +0900

    빠르게 마스터하는 타입스크립트 함수에 타입 지정하는 법 & void 타입

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -26,18 +26,43 @@ let 이름:any;
 // let married:boolean = false; 
 // let 철수:(string|boolean)[] = [user, age, married];
 
-let 학교 : {
-  score : (number|boolean)[],
-  teacher : string,
-  friend : string | string[]
-}
+// let 학교 : {
+//   score : (number|boolean)[],
+//   teacher : string,
+//   friend : string | string[]
+// }
 
-= {
-  score : [100, 97, 84],
-  teacher : 'Phil',
-  friend : 'John'
-}
-학교.score[4] = false;
-학교.friend = ['Lee', 학교.teacher]
+// = {
+//   score : [100, 97, 84],
+//   teacher : 'Phil',
+//   friend : 'John'
+// }
+// 학교.score[4] = false;
+// 학교.friend = ['Lee', 학교.teacher]
 
-console.log(학교.score)
+// console.log(학교.score)
+
+function 함수(x? :number) :void {
+   console.log(x + 3 )
+}
+function sayHi(x? :string ){
+  if (x) {
+    console.log('안녕하세요 ' + x)
+  } else {
+    console.log('왜입력안함')
+  }
+}
+function 결혼가능하냐(money :number, house :boolean, charm :string) :string|void{
+  let score :number = 0;
+  score += money;
+  if (house === true){
+    score += 500
+  }
+  if (charm === '상'){
+    score += 100
+  }
+  if (score >= 600){
+    return '결혼가능'
+  } 
+}
+console.log(결혼가능하냐(100,true,'상'))
