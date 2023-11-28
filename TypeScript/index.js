e07698c274d337c7e8b2bc5e6df500c3becfd660
Oh Junseok <y22zzp@naver.com>
2023-11-28 17:44:21 +0900

    빠르게 마스터하는 타입스크립트 초기 세팅

---
+++ TypeScript/index.js
@@ -0,0 +1,6 @@
+var 이름 = 'kim';
+이름 = "가나다라ee";
+function 함수(x) {
+    return x * 2;
+}
+함수(1234);
---
+++ TypeScript/index.ts
@@ -0,0 +1,9 @@
+let 이름 :string | number = 'kim';
+
+이름 = "가나다라ee";
+
+function 함수(x:number):number {
+  return x * 2 
+}
+
+함수(1234);
