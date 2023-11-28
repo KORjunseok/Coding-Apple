5e73e3248432263fabec30bb6156bfbab81fdf4d
Oh Junseok <y22zzp@naver.com>
2023-11-28 17:59:22 +0900

    빠르게 마스터하는 타입스크립트 타입스크립트 기본 타입 정리 (primitive types)

--- TypeScript/index.js
+++ TypeScript/index.js
@@ -1,5 +1,5 @@
 var 이름 = 'kim';
-이름 = "가나다라ee";
+이름 = "가나다라";
 function 함수(x) {
     return x * 2;
 }
--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,9 +1,16 @@
-let 이름 :string | number = 'kim';
-
-이름 = "가나다라ee";
-
-function 함수(x:number):number {
-  return x * 2 
+let 오준석 : {} = {
+  name : "오준석",
+  age : 29,
+  출생지역 : "인천"
 }
 
-함수(1234);
+let 좋아요 :{제목: string, 가수:string} = {
+  제목 : "예뻤어",
+  가수 : "데이식스"
+}
+
+let project:{member:string[],days:number, started:boolean } = {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true,
+}
