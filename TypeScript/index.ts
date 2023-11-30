9cb6888b4448e24f98223ca422d497956ac98481
Oh Junseok <y22zzp@naver.com>
2023-11-30 20:15:05 +0900

    빠르게 마스터하는 타입스크립트 함수에 사용하는 never 타입도 있긴 합니다

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -215,24 +215,36 @@
 //   }
 // }
 
-type Car = {
-  wheel : '4개',
-  color : string
-}
+// type Car = {
+//   wheel : '4개',
+//   color : string
+// }
 
-type Bike = {
-  wheel : '2개',
-  color : string
-}
+// type Bike = {
+//   wheel : '2개',
+//   color : string
+// }
 
-function 함수(x: Car | Bike){
-  if (x.wheel === '4개'){
-    console.log('x는 Car타입이에요')
+// function 함수(x: Car | Bike){
+//   if (x.wheel === '4개'){
+//     console.log('x는 Car타입이에요')
+//   } else {
+//     console.log('x는 Bike타입이에요')
+//   }
+// }
+
+// naver 타입을 알아보자 
+function 함수(parameter :string){
+  if (typeof parameter == 'string'){
+    console.log(parameter)
   } else {
-    console.log('x는 Bike타입이에요')
+    console.log(parameter)
   }
 }
 
+let 함수2 = function(){
+  throw new Error()
+}
 
 
 
