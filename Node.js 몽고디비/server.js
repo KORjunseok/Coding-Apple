40d3369925d56dabc55c7a680ab05b50c588f10b
Oh Junseok <y22zzp@naver.com>
2023-12-01 19:44:51 +0900

    Node.js, MongoDB로 빠르게 웹서비스 MongoDB와 서버 연결하려면

--- Node.js 몽고디비/server.js
+++ Node.js 몽고디비/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express()
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080, ()=> {
-  console.log('http://localhost:8080 에서 서버 실행 중 ')
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://sparta:test@cluster0.p7hon5t.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('forum')
+  app.listen(8080, ()=> {
+    console.log('http://localhost:8080 에서 서버 실행 중 ')
+  })
+}).catch((err)=>{
+  console.log(err)
 })
 
 app.get('/', (요청, 응답)=> {
@@ -11,13 +21,6 @@ app.get('/', (요청, 응답)=> {
 })
 
 app.get('/news', (req, res) => {
-  res.send('뉴스 볼래')
-})
-
-app.get('/shop', (req, res) => {
-  res.send('쇼핑페이지임')
-})
-
-app.get('/about', (요청, 응답)=> {
-  응답.sendFile(__dirname + '/index2.html')
+  db.collection('post').insertOne({title : '어쩌구'})
+  // res.send('오늘 눈올까?')
 })
