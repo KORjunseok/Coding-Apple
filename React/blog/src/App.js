39613939ac7fc30fcb34d4cffb62cf847111ea5d
오준석 <y22zzp@naver.com>
2023-12-09 22:53:18 +0900

    React 리액트 기초부터 쇼핑몰 프로젝트까지 리액트에서 레이아웃 만들 때 쓰는 JSX 문법 3개

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕 리엑트 처음이야 잘 부탁해
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className= "black-nav">
+      <h4 style = { {color : 'red', fontSize : '16px'}}> 블로그임 </h4>
+     </div>
+     <h4>{post}</h4>
     </div>
   );
 }
