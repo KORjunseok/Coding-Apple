1e97fed51c5f403d7b0b4f2225d1bdb143de7bc8
오준석 <y22zzp@naver.com>
2023-12-11 19:24:07 +0900

    React 리액트 기초부터 쇼핑몰 프로젝트까지! map : 많은 div들을 반복문으로 줄이고 싶은 충동이 들 때

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -9,13 +9,10 @@ function App() {
 
    let post = '강남 우동 맛집';
    let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학'])
-   let [따봉, 따봉변경] = useState(0);
+   let [따봉, 따봉변경] = useState([0,0,0]);
    let [modal, setModal] = useState(false);
 
 
- function 함수(){
-
- }
 
 
   return (
@@ -30,7 +27,7 @@ function App() {
          글제목변경(copy)
       }}> 가나다순정렬 </button>
 
-       <div className = "list"> 
+       {/* <div className = "list"> 
           <button onClick = { ()=> {
             let copy = [...글제목];
             copy[0] = '여자 코트 추천';
@@ -47,7 +44,23 @@ function App() {
        <div className = "list"> 
           <h4 onClick = {() => {setModal(!modal)}}>{글제목[2]}</h4>
           <p>2월 17일 발행</p>
-       </div>
+       </div> */}
+
+
+          {
+            글제목.map(function (a, i) {
+               return(
+                  <div className = "list"> 
+                  <h4 onClick = {() => {setModal(!modal)}}>{글제목[i]} <span onClick={()=>{ 
+                     let copy = [...따봉];
+                     copy[i] = copy[i] + 1;
+                     따봉변경(copy)  
+                  }}>👍</span> {따봉[i]} </h4>
+                  <p>2월 17일 발행</p>
+               </div>
+               )
+            })
+          }
 
          {
             modal == true ? <Modal/> : null           
