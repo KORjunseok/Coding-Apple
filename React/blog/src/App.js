9da3878185d7e8df82cb8a34257040e6c2fab8db
오준석 <y22zzp@naver.com>
2023-12-12 15:20:39 +0900

    React 리액트 기초부터 쇼핑몰 프로젝트까지! 자식이 부모의 state 가져다쓰고 싶을 때는 props

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -63,19 +63,20 @@ function App() {
           }
 
          {
-            modal == true ? <Modal/> : null           
+            modal == true ? <Modal 글제목 = {글제목}/> : null           
          }
 
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
    return (
       <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.글제목[0]}</h4>
        <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
       </div>
    )
 }
