1d4603f0a0c9ac6fb5dddf5f737766f61a5140e8
오준석 <y22zzp@naver.com>
2023-12-12 15:55:03 +0900

    React 리액트 기초부터 쇼핑몰 프로젝트까지! input 1 : 사용자가 입력한 글 다루기

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -12,7 +12,7 @@ function App() {
    let [따봉, 따봉변경] = useState([0,0,0]);
    let [modal, setModal] = useState(false);
    let [title, setTitle] = useState(0);
-
+   let [입력값, 입력값변경] = useState('');
 
 
   return (
@@ -27,31 +27,12 @@ function App() {
          글제목변경(copy)
       }}> 가나다순정렬 </button>
 
-       {/* <div className = "list"> 
-          <button onClick = { ()=> {
-            let copy = [...글제목];
-            copy[0] = '여자 코트 추천';
-            글제목변경(copy)
-          }}> 글수정 </button>
-
-          <h4>{글제목[0]} <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
-          <p>2월 17일 발행</p>
-       </div>
-       <div className = "list"> 
-          <h4>{글제목[1]}</h4>
-          <p>2월 17일 발행</p>
-       </div>
-       <div className = "list"> 
-          <h4 onClick = {() => {setModal(!modal)}}>{글제목[2]}</h4>
-          <p>2월 17일 발행</p>
-       </div> */}
-
 
           {
             글제목.map(function (a, i) {
                return(
                   <div className = "list"> 
-                  <h4 onClick = {() => {setModal(!modal); setTitle(i)}}>{글제목[i]} <span onClick={()=>{ 
+                  <h4 onClick = {() => {setModal(!modal); setTitle(i)}}>{글제목[i]} <span onClick={(e)=>{ e.stopPropagation();
                      let copy = [...따봉];
                      copy[i] = copy[i] + 1;
                      따봉변경(copy)  
@@ -62,7 +43,9 @@ function App() {
             })
           }
 
-
+          <input onChange = {(e) => { 입력값변경(e.target.value)
+            console.log(입력값)
+         }}></input>
 
          {
             modal == true ? <Modal title={title} 글제목 = {글제목}/> : null           
