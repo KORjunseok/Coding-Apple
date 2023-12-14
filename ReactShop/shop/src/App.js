313c720ad6d537381e985cf3f153bb1eab455362
오준석 <y22zzp@naver.com>
2023-12-14 11:58:47 +0900

    React 리액트 기초부터 쇼핑몰 프로젝트까지! 리액트 라우터 2 : navigate, nested routes, outlet~

--- ReactShop/shop/src/App.js
+++ ReactShop/shop/src/App.js
@@ -4,12 +4,12 @@ import { Component, useState } from "react";
 import "./App.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import data from "./data";
-import Detail from "./detail";
-import { Routes, Route, Link } from "react-router-dom";
+import Detail from "./pages/detail";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(data);
-  let [detail] = useState(Detail)
+  let nevigate = useNavigate();
 
   return (
     <div className="App">
@@ -17,14 +17,28 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                nevigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
             {/* <Link to ="/">홈</Link> */}
-            <Nav.Link href="/detail">detail</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                nevigate("/detail");
+              }}
+            >
+              detail
+            </Nav.Link>
             {/* <Link to = "/detail">상세페이지</Link> */}
+            {/* <Nav.Link onClick={ () => {
+              nevigate('/detail')
+            }}>Cart</Nav.Link> */}
           </Nav>
         </Container>
       </Navbar>
-
 
       <Routes>
         <Route
@@ -44,11 +58,41 @@ function App() {
           }
         />
 
-        <Route path="/detail" element={<div> {detail}</div>} />
+        <Route path="/detail" element={<Detail />} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div> 난 멤버임 </div>} />
+          <Route path="location" element={<div> 난 위치정보임 </div>} />
+        </Route>
+
+        <Route path ="/event" element ={<EventPage/>}>
+          <Route path ="one" element ={<p>첫 주문시 양배추즙 서비스</p>} />
+          <Route path ="two" element ={<p>생일기념 쿠폰받기</p>} />
+
+        </Route>
       </Routes>
     </div>
   );
 }
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function EventPage() {
+  return(
+    <div>
+      <h2>오늘의 이벤트</h2>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
 
 function Card(props) {
   return (
