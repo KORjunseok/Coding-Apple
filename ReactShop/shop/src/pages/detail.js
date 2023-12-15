7d630fa0e503f7b2e4bb7d65e14d29a00c10f617
오준석 <y22zzp@naver.com>
2023-12-15 09:36:16 +0900

    React 리액트 기초부터 쇼핑몰 프로젝트까지! styled-components 쓰면 CSS 파일 없어도 되는데

--- ReactShop/shop/src/pages/detail.js
+++ ReactShop/shop/src/pages/detail.js
@@ -1,28 +1,41 @@
 /* eslint-disable */
-import {useParams} from 'react-router-dom'
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: ${ props => props.bg};
+  color:  ${ props => props.bg == 'blue' ? 'white' : 'black'};
+  padding: 10px;
+`;
+
 
 function Detail(props) {
-
-  let {id} = useParams();
-  let 찾은상품 = props.shoes.find(function(x){
-    return x.id == id
+  let { id } = useParams();
+  let 찾은상품 = props.shoes.find(function (x) {
+    return x.id == id;
   });
 
   return (
-<div className="container">
-  <div className="row">
-    <div className="col-md-6">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+    <div className="container">
+
+        <YellowBtn bg="blue">버튼</YellowBtn>
+
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
     </div>
-    <div className="col-md-6">
-      <h4 className="pt-5">{찾은상품.title}</h4>
-      <p>{찾은상품.content}</p>
-      <p>{찾은상품.price}원</p>
-      <button className="btn btn-danger">주문하기</button> 
-    </div>
-  </div>
-</div> 
-  )
+  );
 }
 
-export default Detail
+export default Detail;
