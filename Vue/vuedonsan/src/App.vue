cea7b5bb618b13c3c7f5b1fffaae668f19fb7c7d
오준석 <y22zzp@naver.com>
2023-12-21 14:39:01 +0900

    만들며 배워보는 Vue.js 3 완벽 가이드 리액트보다 100배 쉬운 Vue 반복문 v-for

--- Vue/vuedonsan/src/App.vue
+++ Vue/vuedonsan/src/App.vue
@@ -1,8 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div>
+
+  <div class="menu">
+
+    <a v-for = "a in 메뉴들" :key="a">{{ a }}</a>
+
+  </div>
+
+  <div> 
+    <h4 v-for = "원룸이름 in products" :key ="원룸이름">{{ 원룸이름 }}</h4>
+    <p v-for = "가격 in 가격들" :key ="가격">{{ 가격 }}</p>
+
+  </div>
+  <!-- <div> -->
    <!-- {{ 데이터바인딩 }} -->
-    <h4 >{{ products[0] }}</h4>
+    <!-- <h4 >{{ products[0] }}</h4>
     <p>50 만원</p>
   </div>
   <div>
@@ -12,7 +23,7 @@
   <div>
     <h4 >{{ products[2] }}</h4>
     <p>55 만원</p>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -21,8 +32,9 @@ export default {
   // 데이터 보관함 
   data(){
     return{
-
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      가격들 : [50,60,70]
     }
   },
   components: {},
@@ -36,6 +48,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color : white;
+  padding : 10px
 }
 </style>
