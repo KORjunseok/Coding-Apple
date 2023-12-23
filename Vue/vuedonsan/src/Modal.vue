6e499b52c7de3ba99544679ed2264436cd468d41
오준석 <y22zzp@naver.com>
2023-12-23 21:52:54 +0900

    만들며 배워보는 Vue.js 3 완벽 가이드 사용자의 input을 받는 법 (v-model)

--- Vue/vuedonsan/src/Modal.vue
+++ Vue/vuedonsan/src/Modal.vue
@@ -3,7 +3,8 @@
     <div class="white-bg">
       <h4>{{ 원룸들[누른거].title }}</h4>
       <p>{{ 원룸들[누른거].content }}</p>
-      <p>{{ 원룸들[누른거].price }} 원</p>
+      <input v-model="month">
+      <p> {{ month }}개월 선택함 : {{ 원룸들[누른거].price * month}} 원</p>
       <Discount />
       <button @click="close">닫기</button>
     </div>
@@ -13,6 +14,11 @@
 <script>
 export default {
   name : 'ModalVue',
+  data(){
+    return {
+      month :1,
+    }
+  },
   props : {
     원룸들 : Array,
     누른거 : Number,
