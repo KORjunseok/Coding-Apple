ed8a708b307537092b563ec21b5506efedf67b1e
오준석 <y22zzp@naver.com>
2023-12-26 17:52:27 +0900

    만들며 배워보는 Vue.js 3 완벽 가이드 Blog 프로젝트 생성과 뷰에서 Bootstrap 4, 5 사용법

---
+++ Vue/blog/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">글목록</a>
+        <a class="nav-link" href="#">상세페이지</a>
+        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
