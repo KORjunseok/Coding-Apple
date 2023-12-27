01ff82aaf806d5fe984e761f8f6315fb3291f0f1
오준석 <y22zzp@naver.com>
2023-12-27 15:16:49 +0900

    만들며 배워보는 Vue.js 3 완벽 가이드 뷰 인스타그램 프로젝트 생성 & 레이아웃 만들기

---
+++ Vue/vuestagram/src/components/Container.vue
@@ -0,0 +1,27 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <Post :인스타데이터="인스타데이터"/>
+    <!-- <Post :인스타데이터="인스타데이터"/>
+    <Post :인스타데이터="인스타데이터"/> -->
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue'
+
+export default {
+  name : 'containerVue',
+  props : {
+    인스타데이터 : Array,
+  },
+  components : {
+    Post : Post,
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue/vuestagram/src/components/Post.vue
@@ -0,0 +1,66 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<!-- eslint-disable vue/valid-template-root -->
+<template>
+  <div class="post" v-for="(작명,i) in 인스타데이터" :key="작명">
+    <div class="post-header">
+      <div class="profile"></div>
+      <span class="profile-name">{{인스타데이터[i].name}}</span>
+    </div>
+    <div class="post-body"></div>
+    <div class="post-content">
+      <p>{{인스타데이터[i].likes}}</p>
+      <p><strong>글쓴이아이디</strong> {{인스타데이터[i].content}}</p>
+      <p class="date">{{인스타데이터[i].date}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name : 'postVue',
+  props : {
+    인스타데이터 : Array,
+  }
+};
+</script>
+
+<style>
+.post {
+  width: 100%;
+}
+.profile {
+  background-image: url("https://picsum.photos/100?random=0");
+  width: 30px;
+  height: 30px;
+  background-size: 100%;
+  border-radius: 50%;
+  float: left;
+}
+.profile-name {
+  display: block;
+  float: left;
+  padding-left: 10px;
+  padding-top: 7px;
+  font-size: 14px;
+}
+.post-header {
+  height: 30px;
+  padding: 10px;
+}
+.post-body {
+  background-image: url("https://picsum.photos/600?random=0");
+  height: 450px;
+  background-position: center;
+  background-size: cover;
+}
+.post-content {
+  padding-left: 15px;
+  padding-right: 15px;
+  font-size: 14px;
+}
+.date {
+  font-size: 11px;
+  color: grey;
+  margin-top: -8px;
+}
+</style>
