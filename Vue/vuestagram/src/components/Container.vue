bfc9f0e6d75e353714f526faa8f294691dd99bd2
오준석 <y22zzp@naver.com>
2023-12-27 17:41:55 +0900

    만들며 배워보는 Vue.js 3 완벽 가이드 탭 만들기 & 탭으로 사진 업로드 페이지 만들기

--- Vue/vuestagram/src/components/Container.vue
+++ Vue/vuestagram/src/components/Container.vue
@@ -1,27 +1,94 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div>
-    <Post :인스타데이터="인스타데이터[i]" v-for="(a,i) in 인스타데이터" :key="i"/>
-    <!-- <Post :인스타데이터="인스타데이터"/>
+    <div v-if="step == 0">
+      <Post
+        :인스타데이터="인스타데이터[i]"
+        v-for="(a, i) in 인스타데이터"
+        :key="i"
+      />
+      <!-- <Post :인스타데이터="인스타데이터"/>
     <Post :인스타데이터="인스타데이터"/> -->
+    </div>
+
+    <div v-if="step == 1">
+      <!-- 필터선택페이지 -->
+      <div class="upload-image"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+
+    <div v-if="step == 2">
+      <!-- 글작성페이지 -->
+      <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Post from './Post.vue'
+import Post from "./Post.vue";
 
 export default {
-  name : 'containerVue',
-  props : {
-    인스타데이터 : Array,
+  name: "containerVue",
+  props: {
+    인스타데이터: Array,
+    step: Number,
   },
-  components : {
-    Post : Post,
-  }
-  
-}
+  components: {
+    Post: Post,
+  },
+};
 </script>
 
 <style>
-
+.upload-image {
+  width: 100%;
+  height: 450px;
+  background: cornflowerblue;
+  background-size: cover;
+}
+.filters {
+  overflow-x: scroll;
+  white-space: nowrap;
+}
+.filter-1 {
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+}
+.filters::-webkit-scrollbar {
+  height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.write-box {
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
+}
 </style>
