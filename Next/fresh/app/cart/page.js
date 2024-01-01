3d8e6f1a6a68ff0cb77c2935a7208d34ebeea017
오준석 <y22zzp@naver.com>
2024-01-01 12:23:10 +0900

    Next.js로 웹서비스 만들기  Component에 데이터 전해주려면 props

--- Next/fresh/app/cart/page.js
+++ Next/fresh/app/cart/page.js
@@ -3,24 +3,40 @@
 import Hi from './data2'
 
 export default function Cart() {
+  let 장바구니 = ['Tomatoes', 'Pasta']
   return (
     <div>
       <Hi/>
       <h4 className="title">Cart</h4>
-      <CartItem />
-      <CartItem />
-      <CartItem />
+      <CartItem item={장바구니[0]} />
+      <CartItem item={장바구니[1]} />
+      <Banner content ="롯데카드"/>
+      <Banner content ="현대카드"/>
+      <RedButton color ="Red" content="빨간"/>
+      <RedButton color ="blue" content="파란"/>
 
     </div> 
   );
 }
 
-function CartItem() {
+function Banner(props){
+  return (
+    <h5>{props.content} 결제 행사 중 </h5>
+  )
+}
+
+function CartItem(props) {
   return (
     <div className="cart-item">
-      <p>상품명</p>
+      <p>{props.item}</p>
       <p>$40</p>
       <p>1개</p>
     </div>
   );
+}
+
+function RedButton(props){
+  return (
+    <button style={{ background : props.color }}>{props.content} 버튼</button>
+  )
 }
