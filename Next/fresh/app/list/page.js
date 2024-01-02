71366455e4b23daff491a21eb487f1c8d7c67380
오준석 <y22zzp@naver.com>
2024-01-02 14:44:42 +0900

    Next.js로 웹서비스 만들기 좋아요 버튼 만들기 (useState, onClick)

--- Next/fresh/app/list/page.js
+++ Next/fresh/app/list/page.js
@@ -1,6 +1,11 @@
+'use client'
+
+import {useState} from "react"
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
+  let [수량, 수량변경] = useState(0)
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -9,6 +14,13 @@ export default function List() {
           return <div className="food" key={i}>
             <img src={`/food${i}.png`} className="food-img"/>
           <h4>{상품[i]} $40</h4>
+          <span> {수량} </span>
+          <button onClick={()=>{
+           { 수량변경(수량+1)}
+          }}>+</button>
+                    <button onClick={()=>{
+           { 수량변경(수량-1)}
+          }}>-</button>
         </div>
         })
       }
