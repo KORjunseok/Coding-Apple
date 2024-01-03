1f11ad23c20fbe509fb405d0431cd955bfe46939
오준석 <y22zzp@naver.com>
2024-01-03 11:08:42 +0900

    Next.js로 웹서비스 만들기 상세페이지 만들기 2 (useRouter)

--- Next/forum/app/list/page.js
+++ Next/forum/app/list/page.js
@@ -1,14 +1,17 @@
 import { connectDB } from "@/util/database";
+import Link from "next/link";
+import DetailLink from './DetailLink'
 
 export default async function List() {
   const db = (await connectDB).db("nextforum");
   let result = await db.collection("post").find().toArray();
-
+  console.log(result)
   return (
     <div className="list-bg">
       {result.map((a, i) => (
         <div className="list-item" key={i}>
-          <h4>{result[i].title}</h4>
+          <Link href={'/detail/' + result[i]._id}><h4>{result[i].title}</h4></Link>
+          <DetailLink/>
           <p>{result[i].content}</p>
         </div>
       ))}
