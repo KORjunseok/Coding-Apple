0d3b3dc38560e76d0a1202835d114aebb011c207
오준석 <y22zzp@naver.com>
2024-01-04 15:20:52 +0900

    Next.js로 웹서비스 만들기 삭제기능 만들기 2 (Ajax 추가내용과 에러처리)

--- Next/forum/app/list/ListItem.js
+++ Next/forum/app/list/ListItem.js
@@ -14,12 +14,24 @@ export default function ListItem({ result }) {
 
           <span
             onClick={() => {
-              fetch("/api/test", {
-                method: "POST",
-                body: JSON.stringify([1, 2, 3]),
-              }).then(() => {
-                console.log(123123);
-              });
+              fetch("/api/post/delete", {
+                method: "DELETE",
+                body: result[i]._id,
+              })
+                .then((r) => {
+                  if (r.status == 200) {
+                    return r.json();
+                  } else {
+                    //서버가 에러코드전송시 실행할코드
+                  }
+                })
+                .then((result) => {
+                  //성공시 실행할코드
+                })
+                .catch((error) => {
+                  //인터넷문제 등으로 실패시 실행할코드
+                  console.log(error);
+                });
             }}
           >
             🗑️
