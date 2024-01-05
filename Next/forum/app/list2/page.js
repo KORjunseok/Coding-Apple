a83f8f66e3b19f38e7f1d3aabfa43a99a167b520
오준석 <y22zzp@naver.com>
2024-01-05 14:38:51 +0900

    Next.js로 웹서비스 만들기 회원기능 만들기 : Auth.js 사용한 소셜로그인

--- Next/forum/app/list2/page.js
+++ Next/forum/app/list2/page.js
@@ -1,7 +1,7 @@
 import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
-// 캐싱 20초 
+// 캐싱 결과를 20초 동안 보관하고 사용함. 20초 지나면 URL에 새로 요청
 export const revalidate = 20
 
 export default async function List() {
