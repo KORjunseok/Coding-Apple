0b2a1162c660ce9429fda4d7d9e958e6f1cb98c2
오준석 <y22zzp@naver.com>
2024-01-07 14:46:41 +0900

    Next.js로 웹서비스 만들기 댓글기능 만들기 3

--- Next/forum/app/detail/[id]/Comment.js
+++ Next/forum/app/detail/[id]/Comment.js
@@ -3,10 +3,25 @@ import { useEffect, useState } from 'react'
 
 export default function Comment(props){
   let [comment, setComment] = useState('')
-
+  let [data, setData] = useState([])
+  useEffect(()=>{
+    fetch('/api/comment/list?id=' + props._id).then(r=>r.json()).then((result)=>{
+      setData(result)
+      console.log(data)
+    })
+  },[])
   return (
     <div>
       <div>댓글목록</div>
+      {/* 댓글 목록을 보여주는 map 함수 */}
+      {
+        data.length > 0 ?
+        data.map((a, i)=>{
+          return (
+            <p key={i}>{a.content}</p>
+          )
+        }): '댓글없음'
+      } 
       <input onChange={(e)=>{ setComment(e.target.value)}}/>
       <button onClick={()=>{
         console.log(comment)
